c883b984eef8228e43657142ec670bb3651e6d0c
Jarle Hansen <jarle@jarlehansen.net>
2016-06-15 08:29:49 +0200

    initial commit

---
+++ src/main/java/com/github/springfox/loader/SpringfoxLoaderApplication.java
@@ -0,0 +1,12 @@
+package com.github.springfox.loader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfoxLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfoxLoaderApplication.class, args);
+	}
+}
