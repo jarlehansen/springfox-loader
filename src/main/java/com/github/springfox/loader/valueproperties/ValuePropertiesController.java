7292581d4c07ce80ca7d3d5719eaedfc6fac6b53
jarlehansen <jarle@jarlehansen.net>
2017-04-15 14:00:35 +0200

    added tests for ValuePropertiesController

--- src/main/java/com/github/springfox/loader/valueproperties/ValuePropertiesController.java
+++ src/main/java/com/github/springfox/loader/valueproperties/ValuePropertiesController.java
@@ -2,6 +2,7 @@ package com.github.springfox.loader.valueproperties;
 
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class ValuePropertiesController {
     }
 
     @ApiOperation("List the Value-annotations (springfox-loader)")
-    @RequestMapping(value = "/valueprops", method = RequestMethod.GET)
+    @GetMapping("/valueprops")
     public ResponseEntity<?> getValueProperties() {
         if (valuePropertiesLocator == null) {
             return ResponseEntity.notFound().build();
