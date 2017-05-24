925815b3433e1fbee8919058e15288f2503fa0d2
jarlehansen <jarle@jarlehansen.net>
2017-05-24 18:04:20 +0200

    trying to fix spock issue

--- src/test/groovy/com/github/springfox/loader/SpringfoxLoaderConfigNonActiveProfileSpec.groovy
+++ src/test/groovy/com/github/springfox/loader/SpringfoxLoaderConfigNonActiveProfileSpec.groovy
@@ -4,6 +4,7 @@ import com.github.springfox.loader.testutils.TestApplication
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 import springfox.documentation.spring.web.plugins.Docket
 
