d7169f2939f7ae6fdd819fdbe04e86c06b823ec7
jarlehansen <jarle@jarlehansen.net>
2018-12-18 20:39:31 +0100

    updated dependencies, removed the use of deprecated classes

--- src/main/java/com/github/springfox/loader/ActiveProfilesCondition.java
+++ src/main/java/com/github/springfox/loader/ActiveProfilesCondition.java
@@ -3,6 +3,7 @@ package com.github.springfox.loader;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 public class ActiveProfilesCondition implements Condition {
@@ -10,6 +11,6 @@ public class ActiveProfilesCondition implements Condition {
     public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
         Environment environment = conditionContext.getEnvironment();
         String[] profiles = environment.getProperty("springfox.profiles", String[].class);
-        return profiles == null || profiles.length == 0 || environment.acceptsProfiles(profiles);
+        return profiles == null || profiles.length == 0 || environment.acceptsProfiles(Profiles.of(profiles));
     }
 }
