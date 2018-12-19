605fa5fc23ed62b5bc3150565ece64c79ad3160b
jarlehansen <jarle@jarlehansen.net>
2018-12-19 20:57:19 +0100

    try to avoid unnecessary dependencies

--- src/main/java/com/github/springfox/loader/SpringfoxLoaderProps.java
+++ src/main/java/com/github/springfox/loader/SpringfoxLoaderProps.java
@@ -1,10 +1,8 @@
 package com.github.springfox.loader;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Data
 @Component
 @ConfigurationProperties(prefix = "springfox")
 public class SpringfoxLoaderProps {
@@ -36,5 +34,97 @@ public class SpringfoxLoaderProps {
 
     public String getTitle() {
         return (name == null) ? title : name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getTermsOfServiceUrl() {
+        return termsOfServiceUrl;
+    }
+
+    public void setTermsOfServiceUrl(String termsOfServiceUrl) {
+        this.termsOfServiceUrl = termsOfServiceUrl;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactUrl() {
+        return contactUrl;
+    }
+
+    public void setContactUrl(String contactUrl) {
+        this.contactUrl = contactUrl;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getLicenseUrl() {
+        return licenseUrl;
+    }
+
+    public void setLicenseUrl(String licenseUrl) {
+        this.licenseUrl = licenseUrl;
+    }
+
+    public String getSwaggerUiBasePath() {
+        return swaggerUiBasePath;
+    }
+
+    public void setSwaggerUiBasePath(String swaggerUiBasePath) {
+        this.swaggerUiBasePath = swaggerUiBasePath;
     }
 }
--- src/test/groovy/com/github/springfox/loader/testutils/TestDto.java
+++ src/test/groovy/com/github/springfox/loader/testutils/TestDto.java
@@ -1,13 +1,30 @@
 package com.github.springfox.loader.testutils;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class TestDto {
     private String key;
     private String value;
+
+    public TestDto() {
+    }
+
+    public TestDto(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
